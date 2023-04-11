40fbb89b76739d1a0b0d4c6c1ff337323a4963b0
mehedi9868 <mehedi9868@gmail.com>
2023-04-11 07:38:15 +0600

    filter by functionality added

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -4,6 +4,9 @@ import AppliedDetails from '../AppliedDetails/AppliedDetails';
 
 
 const AppliedJobs = () => {
+    // sort:
+    const [showRemote, setShowRemote] = useState(false);
+
 
     // const allJobs = useLoaderData();
     const [jobs, setJobs] = useState([]);
@@ -19,22 +22,35 @@ const AppliedJobs = () => {
     // get from local storage and match both id:
     const applied = JSON.parse(localStorage.getItem("jobs")) || [];
     const appliedID = applied.map(singleId => singleId.id);
-
     const matchedJobs = jobs.filter(job => appliedID.includes(job._id));
+
+    // sort:
+    const remoteData = matchedJobs.filter((item) => item.remote_or_onsite === "Remote");
+    const onsiteData = matchedJobs.filter((item) => item.remote_or_onsite === "On Site");
+    const handleRemote = () => {
+        setShowRemote(true);
+    };
+    const handleOnsite = () => {
+        setShowRemote(false);
+    };
     return (
         <div className='w-3/4 mx-auto'>
             <h2 className='text-xl font-bold text-center my-10'>Applied Jobs</h2>
             <div className='flex justify-end items-center gap-5 my-6'>
-                <h2 className='text-base font-medium'>Sort By:</h2>
-                <button className="bg-gradient-to-r from-indigo-400 to-purple-500 text-white transition hover:text-black font-bold py-4 px-6 rounded">Remote</button>
-                <button className="bg-gradient-to-r from-indigo-400 to-purple-500 text-white transition hover:text-black font-bold py-4 px-6 rounded">Onsite</button>
+                <h2 className='text-base font-medium'>Filter By:</h2>
+                <button onClick={handleRemote} className="bg-gradient-to-r from-indigo-400 to-purple-500 text-white transition hover:text-black font-bold py-4 px-6 rounded">Remote</button>
+                <button onClick={handleOnsite} className="bg-gradient-to-r from-indigo-400 to-purple-500 text-white transition hover:text-black font-bold py-4 px-6 rounded">Onsite</button>
             </div>
-            {
-                matchedJobs.map(job => <AppliedDetails
+
+            {showRemote
+                ? remoteData.map((job) => <AppliedDetails
                     key={job._id}
                     job={job}
-                ></AppliedDetails>
-                )
+                ></AppliedDetails>)
+                : onsiteData.map((job) => <AppliedDetails
+                    key={job._id}
+                    job={job}
+                ></AppliedDetails>)
             }
         </div>
     );
